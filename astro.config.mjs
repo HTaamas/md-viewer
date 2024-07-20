17347204328b0e0f58890fd86be338c1be90bf60
HTaamas <96864185+HTaamas@users.noreply.github.com>
2024-07-20 20:15:06 +0200

    working on a user changeable md

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), preact()],
+  integrations: [mdx(), preact(), react()],
 });
